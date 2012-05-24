d1029dee0b7dab3194efc0e0b6a99860d63f66f7
Michael Prasuhn <mike@mikeyp.net>
2012-05-23 19:51:48 -0700

    Making routes for looking up IP work

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
+var geoip = require('geoip-lite');
+
+var ip_lookup = function(ip, res) {
+  var result = geoip.lookup(ip);
+  res.send(result);
+}
+
 
 /*
  * GET home page.
@@ -6,3 +13,14 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.api_post = function(req, res){
+  var ip = req.body.ip;
+  ip_lookup(ip, res);
+}
+
+exports.api_get = function(req, res){
+  var ip = req.params.ip;
+  ip_lookup(ip, res);
+}
+
