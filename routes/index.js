9d9c2ce3a228591f444f3438a329ed79e29d815e
Michael Prasuhn <mike@mikeyp.net>
2012-05-24 10:23:41 -0700

    Updating to use mixpanel for tracking

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,38 @@
-var lookup = require('../lib/lookup');
+var lookup = require('../lib/lookup')
+  , mixpanel = require('mixpanel');
 
 /*
  * GET home page.
  */
 
+var mp_client = new mixpanel.Client('ad1b46adcd30b5656d39ae2896f3053f');
+
 exports.index = function(req, res){
+  var client_ip;
+  client_ip = lookup.getClientIp(req);
+  mp_client.track("request", {
+      distinct_id: client_ip,
+      path: "home",
+      mp_note: "Viewed the home page"
+  }, function(err) {
+    
+  });
   res.render('index', { title: 'Geo EC2' })
 };
 
 exports.api_post = function(req, res){
-  var ip;
+  var ip, client_ip;
+  client_ip = lookup.getClientIp(req);
+  mp_client.track("request", {
+      distinct_id: client_ip,
+      path: "api",
+      method: "post",
+      mp_note: "Made an API POST request"
+  }, function(err) {
+    
+  });
   if (typeof(req.body.ip) == 'undefined') {
-    ip = lookup.getClientIp(req);
+    ip = client_ip;
   }
   else {
     ip = req.body.ip; 
@@ -20,9 +41,18 @@ exports.api_post = function(req, res){
 }
 
 exports.api_get = function(req, res){
-  var ip;
+  var ip, client_ip;
+  client_ip = lookup.getClientIp(req);
+  mp_client.track("request", {
+      distinct_id: client_ip,
+      path: "api",
+      method: "get",
+      mp_note: "Made an API GET request"
+  }, function(err) {
+    
+  });
   if (typeof(req.params.ip) == 'undefined') {
-    ip = lookup.getClientIp(req);
+    ip = client_ip;
   }
   else {
     ip = req.params.ip; 
