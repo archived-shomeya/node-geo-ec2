065114e2754ab6e78748353b90f9a53c1ac72c91
Michael Prasuhn <mike@mikeyp.net>
2012-05-24 09:55:08 -0700

    fix lookup for client IP and add help text

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,24 @@ var lookup = require('../lib/lookup');
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Geo EC2' })
 };
 
 exports.api_post = function(req, res){
-  var ip = req.body.ip;
+  var ip;
+  if (typeof(req.body.ip) == 'undefined') {
+    ip = lookup.getClientIp(req);
+  }
+  else {
+    ip = req.body.ip; 
+  }
   lookup.lookup(ip, res);
 }
 
 exports.api_get = function(req, res){
   var ip;
   if (typeof(req.params.ip) == 'undefined') {
-    ip = req.connection.remoteAddress;
+    ip = lookup.getClientIp(req);
   }
   else {
     ip = req.params.ip; 
